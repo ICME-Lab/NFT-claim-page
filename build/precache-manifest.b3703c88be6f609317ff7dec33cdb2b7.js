45aeb89a178293fa9420f82d90ab388cbf76fe60
WYATT <wyattbenno@gmail.com>
2023-04-30 10:12:30 +0900

    all of these files are needed to make it work with first install..

---
+++ build/precache-manifest.b3703c88be6f609317ff7dec33cdb2b7.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed216209caf482b8b78f658d69f5114a",
+    "url": "/index.html"
+  }
+];
